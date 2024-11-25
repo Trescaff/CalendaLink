267907e4f56ed965072e86c79014ff369c631035
unknown <afqh262@gmail.com>
2024-11-25 16:10:44 +0900

    edit

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const cors = require('cors'); //new
+const bodyParser = require('body-parser'); //new
 
 const app = express();
 const port = 5000;
 
+// Middleware part new
+app.use(cors());
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+// Sample API Route new
+app.get('/api', (req, res) => {
+  res.json({ message: 'Backend connected successfully!' });
 });
 
 app.listen(port, () => {
