6012bfe6e3a9a6b296fc1c10a667fdf13b40f36d
unknown <afqh262@gmail.com>
2024-11-27 21:10:07 +0900

    edit

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,26 +1,6 @@
-const express = require('express');
-const cors = require('cors'); //new
-const bodyParser = require('body-parser'); //new
+const app = require('./app');
 
-const app = express();
-const port = 5000;
-
-// Middleware part new
-app.use(cors());
-app.use(bodyParser.json());
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
-
-// Sample API Route new
-app.get('/api', (req, res) => {
-  res.json({ message: 'Backend connected successfully!' });
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
-
-//test pull
-//test push
